58efabb5aa529e3ff1580a36cf059888e66824fd
sumsumcorn <nicecorntea@naver.com>
2023-11-16 23:13:45 +0900

    langchain 작동테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import { OpenAI } from 'langchain/llms/openai';
+import { ChatOpenAI } from 'langchain/chat_models/openai';
 function App() {
-  return <div>init</div>;
+  const [answer, setAnswer] = useState('');
+  useEffect(() => {}, []);
+
+  const generateJoke = async () => {
+    const chatModel = new ChatOpenAI({
+      openAIApiKey: import.meta.env['VITE_OPENAI_API_KEY'],
+    });
+    const chatResult = await chatModel.predict(
+      'Say something funny jokes. Speak in Korean.'
+    );
+
+    setAnswer(chatResult);
+  };
+  return (
+    <div>
+      <div>test</div>
+      <button onClick={generateJoke}>click me!</button>
+      <div>{answer}</div>
+    </div>
+  );
 }
 
 export default App;
